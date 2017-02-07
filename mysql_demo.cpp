346582fefd812a117ba5c131c8a55905d18dd865
Nikesh Joshi <nikeshjoshi2001@gmail.com>
2017-02-07 14:00:44 +0530

    Add files via upload

---
+++ mysql_demo.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+using namespace std;
+int main()
+{
+    try
+    {
+        sql::Driver *driver;
+        sql::Connection *con;
+        sql::Statement *stmt;
+        sql::ResultSet *res;
+      /* Create a connection */
+      driver = get_driver_instance();
+      con = driver->connect("tcp://127.0.0.1:3306", "root", "root");
+      /* Connect to the MySQL test database */
+      con->setSchema("test");
+      stmt = con->createStatement();
+      res = stmt->executeQuery("SELECT 'Hello World!' AS _message");
+      while (res->next()) {
+        cout << res->getString("_message") << endl;
+        cout << res->getString(1) << endl;
+      }
+      delete res;
+      delete stmt;
+      delete con;
+
+    }
+    catch (sql::SQLException &e)
+    {
+        cout << "# ERR: SQLException in " << __FILE__;
+        cout << "(" << __FUNCTION__ << ") on line "
+        << __LINE__ << endl;
+        cout << "# ERR: " << e.what();
+        cout << " (MySQL error code: " << e.getErrorCode();
+        cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+    }
+    cout << endl;
+    return 0;
+}
